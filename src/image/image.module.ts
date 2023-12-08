4f17dffb6f221fe7344c7a7202a030efc5f7ffda
DeepL1x <potato1869@gmail.com>
2023-12-08 17:18:13 +0200

    refactoring: delete unused code and packages

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ImageController } from './image.controller';
-import { GridFsMulterConfigService } from './multer-config.service';
 import { ImageService } from './image.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SharpModule } from 'nestjs-sharp';
@@ -8,7 +7,7 @@ import { SharpModule } from 'nestjs-sharp';
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO), SharpModule],
   controllers: [ImageController],
-  providers: [GridFsMulterConfigService, ImageService],
+  providers: [ImageService],
   exports: [ImageService],
 })
 export class ImageModule {}
